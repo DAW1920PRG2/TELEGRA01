bf721d7e9c1fdfcd237a1d45ec756c3d7a6806a3
DAW1920PRG2 <amisecretaria@hotmail.com>
2021-03-15 10:30:33 +0100

    pongo textos

--- CodigoTelegram2/CodigoTelegram2/Form1.Designer.cs
+++ CodigoTelegram2/CodigoTelegram2/Form1.Designer.cs
@@ -40,18 +40,18 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(137, 38);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(150, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "introduce texto para telegrama";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(140, 201);
+            this.label2.Location = new System.Drawing.Point(137, 212);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(33, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.label2.Text = "coste";
             // 
             // textBox1
             // 
@@ -74,7 +74,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
+            this.button1.Text = "calcular";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
@@ -88,7 +88,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Introduce texto para telegrama";
             this.ResumeLayout(false);
             this.PerformLayout();
 
